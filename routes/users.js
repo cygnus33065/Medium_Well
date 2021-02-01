0506956c91221aeb0831913e49199cf6e62fe93d
olivianicole <onyoung@umich.edu>
2021-02-01 16:58:15 -0500

    user post request & validators

--- routes/users.js
+++ routes/users.js
@@ -2,20 +2,30 @@ var express = require('express');
 const asyncHandler = require('../utils.js')
 const { compileClientWithDependenciesTracked } = require('pug');
 var router = express.Router();
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs');
+const db = require('../db/models')
+const { User } = db;
+const csrfProtection = require('../utils.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/signup', asyncHandler(async(req, res) => {
+router.post('/signup', csrfProtection, asyncHandler(async(req, res) => {
   const {username, email, password, confirmPassword} = req.body;
   if (password === confirmPassword){
-    const hashedPassword = await bcrypt.hash(password, 8)
+    const hashedPassword = await bcrypt.hash(password, 8);
+    await User.create({
+      username,
+      email,
+      hashedPassword
+    });
     req.session.save(() =>{
-      res.redirect('/')
-    })
+      res.redirect('/');
+    });
+  } else {
+
   }
   
 }));
--- utils.js
+++ utils.js
@@ -1,4 +1,34 @@
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+const csrf = require('csurf');
+const csrfProtection = csrf({ cookie: true });
+const { check, validationResult } = require('express-validator');
+const errorHandler = (req, res, next) => {
+    const validationErrors = validationResult(req);
+    if (!validationErrors.isEmpty()){
+        const errors = validationErrors.array().map((error) => error.msg);
+        const err = Error('Bad Request');
+        err.errors = errors;
+        err.status = 400;
+        err.title = 'Bad Request';
+        return next(err);
+    } next();
+}
+
+const userValidator = [
+    check('username')
+        .exists({ checkFalsy: true })
+        .isLength({ max: 20 })
+        .withMessage('Invalid Username'),
+    check('email')
+        .exists({ checkFalsy: true })
+        .isLength({ max: 50 })
+        .isEmail()
+        .withMessage('Invalid Email Address'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .isStrongPassword() 
+        .withMessage("Password Doesn't Meet Requirements")
+];
 
 
-module.exports = asyncHandler;
+module.exports = asyncHandler, csrfProtection;
