1b9ed12b28735a171985f89472e5c9bc927fa1b5
cygnus33065 <cygnus_33065@yahoo.com>
2021-02-02 16:23:18 -0500

    worked on uniqueness checking

--- utils.js
+++ utils.js
@@ -12,20 +12,26 @@ const errorHandler = (req, res, next) => {
     if (!validationErrors.isEmpty()){
         req.errors = validationErrors.array().map((error) => error.msg);
 
-    } 
+    }
     next();
 }
 
 const checkUnique = async (username, email) => {
-   
+
     const user = await User.findAll({
         attributes: ['username', 'email']
     });
-    console.log(user);
-    if (user.dataValues.username.includes(username)){
+    const usernamesAndEmails = []
+
+    for (let i = 0; i < user.length; i++){
+        usernamesAndEmails.push(current.dataValues.username)
+        usernamesAndEmails.push(current.dataValues.email)
+    }
+    console.log(usernamesAndEmails)
+    if (usernamesAndEmails.includes(username)){
         errors.push('Username is Taken')
     }
-    if (user.dataValues.email.includes(email)){
+    if (usernamesAndEmails.includes(email)){
         errors.push("Email Already in Use")
     }
 };
