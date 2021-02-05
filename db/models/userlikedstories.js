139c35828a7d102f97938e1eeb4ced95fdf4de87
Rheannone <rheannone@gmail.com>
2021-02-04 19:23:26 -0500

    wrote most story routes

--- db/models/userlikedstories.js
+++ db/models/userlikedstories.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
     storyId: DataTypes.INTEGER
   }, {});
   UserLikedStory.associate = function(models) {
-    // associations can be defined here
+    UserLikedStory.belongsTo(models.Story, {foreignKey: "storyId"})
   };
   return UserLikedStory;
 };
--- routes/test.js
+++ routes/test.js
@@ -27,67 +27,18 @@ router.get('/storycategories', asyncHandler(async(req, res, next) =>{
     res.json(users)
 }))
 
-// router.get('/stories', asyncHandler(async(req, res, next) =>{
-//     const users = await Story.findAll()
-//     res.json(users)
-// }))
-
 router.get('/followers', asyncHandler(async(req, res, next) =>{
     const users = await Follower.findAll()
     res.json(users)
 }))
 
-// STORY ROUTES
-
-// all stories - brandon and john wrote this one
-router.get('/stories', asyncHandler(async(req, res, next) =>{
-    const users = await Story.findAll()
-    res.json(users)
-}));
-
-// // one story 
-router.get('/stories/:id', asyncHandler(async(req, res, next) =>{
-    const id = parseInt(req.params.id, 10)
-    const story = await Story.findByPk(id)
-    res.json(story);
-}))
-// // Today's Articles - 
+//  DOES NOT WORK in story.js but works in test.js
 router.get('/recent', asyncHandler(async(req, res, next) => {
     const story = await Story.findAll({
         order: [["date", "DESC"]]
     })
     res.json(story)
 }))
-// Random Story - can't tell if this works or not 
-router.get('/random', asyncHandler(async(req, res, next) => {
-    const story = Story.findAll({
-        order: [
-            Sequelize.fn( 'RANDOM' ),
-          ]
-    })
-    res.json(story)
-}));
-// Trending Stories - best way to implement? .count() or maybe .findAndCountAll()
-// router.get('/trending', asyncHandler(async(req, res, next) => {
-//     const likedStories = Story.findAll({
-//         include: [{model: UserLikedStory}],
-//         order: [
-//             ['models.UserLikedStory', 'ASC']
-//         ]
-//     })
-//     res.json(likedStories)
-// }))
-
-// router.get('/trending', asyncHandler( async (req, res, next) => {
-//     const id = parseInt(req.params.id, 10)
-//     const mostLikes = await UserLikedStory.findAll({
-//         where: {storyId: id},
-//         limit: 5
-//     })
-//     // const likedStories = await Story.findByPk({
-//     //     where: {id : mostLikes}
-//     // })
-// }))
 
 
 
