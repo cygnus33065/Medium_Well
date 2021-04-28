218350823cbe7b37a000ab11df89ed3d852eb781
cygnus33065 <cygnus_33065@yahoo.com>
2021-04-28 17:57:32 -0400

    got likes working with an AJAX method

--- routes/story.js
+++ routes/story.js
@@ -21,7 +21,7 @@ router.get('/random', asyncHandler(async(req, res, next) => {
 router.get('/trending', asyncHandler( async (req, res, next) => {
    // count number of times a storyID appears on the UserLikedStories table
    // find the top X most frequent storyID's
-   // query the Stories table for the id's from the previous query. 
+   // query the Stories table for the id's from the previous query.
     const story = await Story.count({
         where: {
             author: "Olivia Young"
@@ -36,7 +36,7 @@ router.get('/', asyncHandler(async(req, res, next) =>{
     res.json(stories)
 }));
 
-// // one story 
+// // one story
 router.get('/:id', asyncHandler(async(req, res, next) =>{
     const storyId = parseInt(req.params.id, 10);
     const likeCount =  await UserLikedStory.count({where: {storyId}})
@@ -62,7 +62,7 @@ router.get('/categories/:id/stories', asyncHandler(async(req, res, next) =>{
     const id = parseInt(req.params.id, 10)
     const stories = await Category.findByPk(id, {
         include: {model: Story}
-    }) 
+    })
 
     const comments = await Comment.findAll({
         where: {
@@ -92,16 +92,19 @@ router.post('/:id/like', asyncHandler(async(req,res,next)=> {
         },
     });
     const isLiked = await UserLikedStory.findOne({where: {storyId, userId: user.id}})
+    console.log(isLiked)
     if(!isLiked){
-        await UserLikedStory.create({
+         await UserLikedStory.create({
             userId: user.id,
             storyId
         })
     } else {
-        await UserLikedStory.destroy({ where: {userId: user.id, storyId}})
+         await UserLikedStory.destroy({ where: {userId: user.id, storyId}})
     }
-    const likeCount =  await UserLikedStory.count({where: {storyId}})
-    res.render('story', {story, categories, comments, likeCount})
+
+    const likes =  await UserLikedStory.count({where: {storyId}})
+    return res.json({likes})
+
 }))
 
 router.post('/:id', asyncHandler(async(req,res,next)=> {
@@ -109,7 +112,7 @@ router.post('/:id', asyncHandler(async(req,res,next)=> {
     const story = await Story.findByPk(storyId);
     const categories = await Category.findAll();
     const username = 'demo'; // hardcoding demo user - come back to select current user
-    const user = await User.findOne({ where: {username}}) 
+    const user = await User.findOne({ where: {username}})
     const {comment} = req.body; // grabbing comment
     const likeCount =  await UserLikedStory.count({where: {storyId}})
 
@@ -118,12 +121,14 @@ router.post('/:id', asyncHandler(async(req,res,next)=> {
       userId: user.id,
       storyId
     })
-    const comments = await Comment.findAll({
-        where: {
-            storyId
-        },
-    });
-    res.render('story', {story, categories, comments, likeCount})
+
+    return res.json({newComment})
+    // const comments = await Comment.findAll({
+    //     where: {
+    //         storyId
+    //     },
+    // });
+    // res.render('story', {story, categories, comments, likeCount})
 }));
 
 module.exports = router;
